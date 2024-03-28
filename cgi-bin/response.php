f038e82e8da92b34e15915ea24fce1955eed1223
Aurore DUCOBU <aducobu@paul-f4Br5s7.clusters.42paris.fr>
2024-03-28 11:09:34 +0100

    design features & response ok

--- cgi-bin/response.php
+++ cgi-bin/response.php
@@ -1,21 +1,26 @@
 <?php
     $query = $_SERVER["QUERY_STRING"];
-    $login = $_GET["login"];
-    echo "La valeur du champ login est : " . $login;
+    $params = array();
+    parse_str($query, $params);
     echo '<!DOCTYPE html>
     <html lang="en">
-        <head>
-            <link href="/style/style.css" rel="stylesheet">
-            <link href="/style/response.css" rel="stylesheet">
-            <title>Response</title>
-        </head>
-        <body>
-            <h1 class="pagesTitle">Response</h1>
-            <p> test </p>
-            <p>' . $query . '</p>
-            <div class="index">
-                <a class="indexButton" href="/site_index.html">Index</a>
-            </div>
-        </body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/style/style.css" rel="stylesheet">
+        <link href="/style/response.css" rel="stylesheet">
+        <title>Response</title>
+    </head>
+    <body>
+        <h1 class="pagesTitle">Response</h1>
+        <div class="response">
+            <div class="tag">Name : <span class="value">' .$params['name'] . '</span></div>
+            <div class="tag">Firstname : <span class="value">' .$params['firstname'] . '</span></div>
+            <div class="tag">Login : <span class="value">' .$params['login'] . '</span></div>
+        </div>
+        <div class="index">
+            <a class="indexButton" href="/site_index.html">Index</a>
+        </div>
+    </body>
     </html>';
 ?>
